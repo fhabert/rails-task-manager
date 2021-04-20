2f0ded787ea40459de235fe8dd3f5416ac5a5002
felix habert <uclqfbf@ucl.ac.uk>
2021-04-21 00:29:17 +0100

    tasks done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "Mousse au chocolat", details: "I love it", completed: "true")
+Task.create(title: "Mousse au chocolat", details: "I love it", completed: "true")
+Task.create(title: "Charlotte fraise", details: "I love it", completed: "true")
